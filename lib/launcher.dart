50f6919ca601dcaeff11e51cf28ec94c8117b5c8
Widraaa <ardiwidiyanto12@gmail.com>
2022-11-15 12:50:13 +0700

    First Commit

---
+++ lib/launcher.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+fluter class Launcer extends StatefulWidget {
+
+  @override
+  State<Launcer> createState() => _LauncerState();
+}
+
+class _LauncerState extends State<Launcer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
