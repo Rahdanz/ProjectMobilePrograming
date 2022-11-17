9b41ebb52a47d1130950f66a157797649d946b0e
Rahdanz <rahdan18@gmail.com>
2022-11-17 12:41:56 +0700

    tes

--- lib/launcher.dart
+++ lib/launcher.dart
@@ -5,10 +5,10 @@ fluter class Launcer extends StatefulWidget {
   @override
   State<Launcer> createState() => _LauncerState();
 }
-
 class _LauncerState extends State<Launcer> {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+  // tanda
 }
